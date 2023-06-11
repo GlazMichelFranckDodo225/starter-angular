ba83d0ee278606bddce2b4896437a564c14e3abc
FranckDodo91 <devlogappdgmf@gmail.com>
2023-06-11 06:21:32 +0200

    Project Initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
+// MODULES RACINE REGROUPANT TOUS LES AUTRES MODULES
+// Module ==> regroupement de composants par fonctionnalités
+// Imports de modules natifs à Angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Import du module contenant les routes
 import { AppRoutingModule } from './app-routing.module';
+// Import du composant "AppComponent"
 import { AppComponent } from './app.component';
 
+// Déclarations des modules
 @NgModule({
-  declarations: [
+  // Liste de tous les composants appartenant au module
+  declarations: [ 
     AppComponent
   ],
+  // Tous les modules à importer 
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  // Système d'injection de dépendances
   providers: [],
+  // Composant racine ==> 1er composant à lancer au 
+  // chargement de l'application
   bootstrap: [AppComponent]
 })
 export class AppModule { }
